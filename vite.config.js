c436eaf5522369b7e6a44b96823e0ee1f33a42ff
Andrey <0test@mail.ru>
2024-01-23 17:08:32 +0300

    init

---
+++ vite.config.js
@@ -0,0 +1,49 @@
+const path = require('path')
+import { resolve } from 'path';
+
+import handlebars from 'vite-plugin-handlebars';
+
+export default {
+	base: '',
+	root: path.resolve(__dirname, 'src'),
+	build: {
+		
+		rollupOptions: {
+			input: {
+				index: path.resolve(__dirname, 'src/index.html'),
+				profile: path.resolve(__dirname, 'src/text.html'),
+			},
+			output: {
+				assetFileNames: (assetInfo) => {
+					let extType = assetInfo.name.split('.').at(1);
+					let ext = assetInfo.name.split('.').pop()
+	
+					if (/png|jpe?g|svg|gif|tiff|bmp|ico/i.test(extType) || /svg/i.test(ext)) {
+						extType = 'img'
+					} else if (/webfonts|fonts/i.test(extType) || /ttf|woff2/i.test(ext)) {
+						extType = 'fonts'
+					}
+					else if(/css/i.test(extType) || /css/i.test(ext)){
+						extType = 'css'  
+					}
+					return `assets/${extType}/[name].[hash][extname]`
+				},
+				chunkFileNames: 'assets/js/[name].[hash].js',
+				entryFileNames: 'assets/js/[name].[hash].js'
+			}
+		},
+		outDir: path.resolve(__dirname, 'dist'),
+		assetsDir: 'assets',
+		
+
+	},
+	server: {
+		port: 8080,
+		hot: true
+	},
+	plugins: [
+		handlebars({
+			partialDirectory: resolve(__dirname, 'src/partials'),
+		}),
+	],
+}
